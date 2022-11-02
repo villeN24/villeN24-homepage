d1d8d3ad2375103b3077ac7cd9f6a65495bfaf10
Ville Naalisvaara <ville.naalisvaara@tuni.fi>
2022-11-02 17:46:06 +0200

    Test if updates go through.

--- src/App.tsx
+++ src/App.tsx
@@ -23,6 +23,7 @@ function App() {
         <p>
           Edit <code>src/App.tsx</code> and save to test HMR
         </p>
+        <p>Test change to see if things go through.</p>
       </div>
       <p className="read-the-docs">
         Click on the Vite and React logos to learn more
