d0cb016373bfb7388d03a85a0e0431c57eb1901f
Ville Naalisvaara <ville.naalisvaara@tuni.fi>
2022-11-02 18:40:08 +0200

    Add basic routing.

--- src/App.tsx
+++ src/App.tsx
@@ -1,33 +1,25 @@
 import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import './App.css'
+import {Routes, Route, Link} from "react-router-dom"
+import Certificates from './pages/Certificates'
+import Home from './pages/Home'
+import Projects from './pages/Projects'
 
 function App() {
-  const [count, setCount] = useState(0)
 
   return (
     <div className="App">
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src="/vite.svg" className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://reactjs.org" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-        <p>Test change to see if things go through.</p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
+      <nav>
+        <div>
+          <Link to="/">Home</Link>
+          <Link to="/projects">Projects</Link>
+          <Link to="/certificates">Certificates</Link>
+        </div>
+      </nav>
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/projects" element={<Projects />} />
+        <Route path="/certificates" element={<Certificates />} />
+      </Routes>
     </div>
   )
 }
---
+++ src/pages/Certificates.tsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+function Certificates() {
+  return (
+    <div>certificates</div>
+  )
+}
+
+export default Certificates
